f36cb2d251ca18126870906e78a14fc3f2c5cb7d
TVARA <TVARA@DESKTOP-C0GFB9B>
2021-10-01 14:58:03 +0100

    Adicionar arquivos de projeto.

---
+++ Formulario1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox3.Text);
+            listBox2.Items.Add(DateTime.Now.Date.ToShortDateString());
+            listBox2.Items.Add(DateTime.Now.TimeOfDay.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox2.Clear();
+        }
+    }
+}
